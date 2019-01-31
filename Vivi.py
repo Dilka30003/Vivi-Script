a32e60991253bcada75da30d26bad89a06ef28a1
Unknown <dhiluka.mapa@hotmail.com>
2019-02-01 08:39:56 +1100

    Added Files

---
+++ Vivi.py
@@ -0,0 +1,40 @@
+from pyautogui import press, typewrite, hotkey
+import keyboard
+from keyboard import press
+import time
+
+x = 0
+
+#time.sleep(5)
+ 
+while x < 10000:
+    keyboard.wait('esc')
+    for i in range(0, 100):
+        y = str(x).zfill(4)
+
+    #    print(y)
+    #    typewrite(y)
+    #    hotkey('enter')
+    #    hotkey('backspace')
+    #    hotkey('backspace')
+    #    hotkey('backspace')
+    #    hotkey('backspace')
+        keyboard.write(y)
+        time.sleep(0.01)
+        hotkey('enter')
+        time.sleep(0.01)
+
+        press('backspace')
+        time.sleep(0.01)
+        press('backspace')
+        time.sleep(0.01)
+        press('backspace')
+        time.sleep(0.01)
+        press('backspace')
+        time.sleep(0.01)
+        press('backspace')
+        time.sleep(0.01)
+        press('backspace')
+
+        x = x + 1
+        time.sleep(0.05)
